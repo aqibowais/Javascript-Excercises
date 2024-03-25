96dbfdb35c471ad25d93c0fa11f9b44539eb0f02
Aqib Owais <owaisharoon60@gmail.com>
2024-03-26 02:27:42 +0500

    Chp 3 Solutions

---
+++ Chapter 3/chp3.js
@@ -0,0 +1,15 @@
+// chp 3
+
+//Question : 1
+var age = 20 ;
+ alert("I am " + age + " years old. ")
+
+ //Question : 3
+ var birthDay = 2003. ;
+ document.write("My birth Year is " + birthDay + "<br>" + " Data type of my declared variable is number.")
+
+ //Question : 4
+ var visitorName = "Aqib Owais"
+ var productName = "T-Shirt (M)"
+ var productQuantity = 5
+ document.write("<br>" + visitorName + " Ordered " + productQuantity + productName + " on XYZ clothing store.")
