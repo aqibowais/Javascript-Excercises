2a3f3c165585fcdbeeb6f7b369d15341f5eff330
Aqib Owais <owaisharoon60@gmail.com>
2024-03-26 02:14:34 +0500

    Chp 1 Solutions

---
+++ chp1.js
@@ -0,0 +1,8 @@
+//ch no : 01
+
+alert('Error! Please Enter a Valid Password.');
+alert ('Welcome to JS land...\n Happy Coding');
+alert('Welcome to JS land');
+alert('Happy Coding! \n\n Prevent this page from creating additional dialogs');
+// alert.
+console.log('Hello.. I can run js through my web browers console');
