ef21848b4f036f6c402fc5b5763d48bf93538f79
Aqib Owais <owaisharoon60@gmail.com>
2024-03-26 02:23:47 +0500

    Chp 2 Solutions

---
+++ Chapter 2/chp2.js
@@ -0,0 +1,51 @@
+//ch : 2 
+//Question : 1
+var username;
+
+//Question : 2
+var nyName = 'Aqib Owais';
+
+
+//Question : 3 (A)
+var message ;
+
+//Question : 3 (B)
+message = "Hello World";
+
+
+//Question : 3 (C) 
+alert(message);
+
+console.log(message);
+
+//Question :4
+var studentAge = "20 years old" 
+
+alert(studentAge)
+
+var course = "Web and Hybrid Mobile App Development"
+alert(course)
+
+//Question : 5
+var Menu = "Pizza \npizz \npiz \npi \np"
+alert(Menu)
+
+//Question : 6
+var email = "aqibowais60@gmail.com"
+alert("My email address is " + email)
+
+
+//Question : 7
+var Book = "A smarter way to learn JavaScript"
+alert("I am trying to learn from the book " + Book);
+
+//Question : 8 
+var htmlCont = "Yah! i can write HTML content through javascript"
+document.write(htmlCont , "<br>")
+
+
+//Question : 9
+var Design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+
+alert(Design) 
+document.write(Design)
