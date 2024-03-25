6412f92d6761b20249f4aeb3dbbd58218d5cb148
Aqib Owais <owaisharoon60@gmail.com>
2024-03-26 02:34:57 +0500

    Chp 3 and 4 Solutions

---
+++ Chapter 3 and 4/chp4.js
@@ -0,0 +1,37 @@
+//Chp : 4
+//Question : 1
+
+var a, b, c ;
+
+
+//Question : 2
+
+// legal variable name
+// myVar
+// firstName
+// _count
+// $result
+// camelCaseVariable
+
+// illegal variable name
+
+// 123var 
+// var-name 
+// va r 
+// var
+// true
+
+
+//Question : 3
+document.write(
+    "<h2>Legal variable name </h2>"+"<br>"+ " myVar " + "<br>" + "firstName" + "<br>" + "count" + "<br>" + "$result" + "<br>" + "camelCaseVariable" + "<br>" + "<h2>Illegal variable name</h2>" + "<br>" + "123var" + "<br>" + "var-name" + "<br>"  + "va r" +"<br>" + "vartrue" + "<br>" + "true")
+
+
+//Question : 4
+document.write("<br><br>" + "<h1>Rules  for naming JS variable</h1>")
+document.write("Variable names can only contain letters, numbers, underscores, and dollar signs.<br>");
+document.write("For example $my_1stVariable<br>");
+document.write("Variables must begin with a letter, underscore, or dollar sign.<br>");
+document.write("For example $name, _name, or name<br>");
+document.write("Variable names are case sensitive.<br>");
+document.write("Variable names should not be JavaScript keywords.<br>");
